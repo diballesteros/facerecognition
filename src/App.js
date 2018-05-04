e03aa7464461886b5822f353df1f19d4f4b2d14a
Diego Ballesteros <diballesterosc@gmail.com>
2018-05-04 17:23:03 -0500

    Heroku routing

--- src/App.js
+++ src/App.js
@@ -103,7 +103,7 @@ class App extends Component {
   // Upon submitting URL use clarifai API method from the BACKEND to receive facebox coordinates
   onButtonSubmit = () => {
     this.setState({ imageUrl: this.state.input });
-    fetch('http://localhost:3000/imageurl', {
+    fetch('https://arcane-savannah-46733.herokuapp.com:3000/imageurl', {
       method: 'post',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
@@ -113,7 +113,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('http://localhost:3000/image', {
+          fetch('https://arcane-savannah-46733.herokuapp.com/image', {
             method: 'put',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
